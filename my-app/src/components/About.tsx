aeb1c2c8d1ec8a52bc207c7845f5bc78f8470107
PabloSantos-CG <comercial-pablo@outlook.com>
2024-04-09 18:15:50 -0300

    feat: add About component

---
+++ my-app/src/components/About.tsx
@@ -0,0 +1,48 @@
+export default function About() {
+  return (
+    <section id="About">
+      <div className="h-screen container m-auto flex gap-x-4">
+        <div className="flex-1 flex flex-col justify-center items-center">
+          <p className="flex flex-col">
+            <span className="text-gray-600 font-bold text-xl">DESCRUBA A</span>
+            <span className="text-blue-700 font-bold text-xl">
+              EXCÊLENCIA EM INTERNET
+            </span>
+            <span className="text-gray-600 font-bold text-xl">
+              SUA MELHOR ESCOLHA
+            </span>
+            <span className="text-blue-700 font-bold text-xl">
+              PARA CONEXÃO CONFIÁVEL
+            </span>
+            <span className="text-gray-600 font-bold text-xl">
+              RÁPIDA E ESTÁVEL.
+            </span>
+          </p>
+        </div>
+
+        <div className="flex-1 flex flex-col justify-center">
+          <h2 className="text-2xl font-semibold mb-6">Sobre Nós</h2>
+          <p className="text-sm text-gray-600 max-w-md">
+            A VirtualLink é um provedor de internet com mais de 17 anos de
+            experiência no mercado, oferecendo serviços de alta qualidade aos
+            nossos clientes.
+            <br />
+            <br />
+            Somos uma empresa certificada pela ANATEL, com contrato com a COELBA
+            para utilização de postes e uma equipe de engenharia dedicada à
+            segurança e capacitação dos nossos colaboradores.
+            <br />
+            <br />
+            Nossa rede de internet abrange mais de 250 km de fibra óptica,
+            atendendo às regiões de Salvador, Camaçari e Lauro de Freitas.
+            <br />
+            <br />
+            Nosso compromisso é proporcionar a melhor experiência de conexão de
+            internet com ultravelocidade, garantindo a diversão da sua família e
+            a sua produtividade no trabalho.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
