e2d818af0b632316ac87792b813fe0253f3ae5aa
PabloSantos-CG <comercial-pablo@outlook.com>
2024-04-11 19:21:37 -0300

    feat: add ourplans component

--- my-app/src/components/OurPlans.tsx
+++ my-app/src/components/OurPlans.tsx
@@ -1,11 +1,12 @@
 import Card from "./slide-components/Card";
+import PlansSlide from "./slide-components/PlansSlide";
 
 export default function OurPlans() {
   return(
-    <section id="OurPlans" className="h-[600px]">
-      <div className="container m-auto flex flex-col justify-center">
-        <h2>Nossos Planos</h2>
-        <Card />
+    <section id="OurPlans" className="h-[600px] flex justify-center items-center">
+      <div className="container m-auto px-4">
+        <h2 className="text-white text-3xl text-center font-semibold mb-12">Nossos Planos</h2>
+        <PlansSlide />
       </div>
     </section>
   );
