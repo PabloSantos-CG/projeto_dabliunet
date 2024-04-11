e97e15da1cd4e9863ca950e33a67ba77ffdaa1a4
PabloSantos-CG <comercial-pablo@outlook.com>
2024-04-11 19:22:21 -0300

    feat: add plansslides component

---
+++ my-app/src/components/slide-components/PlansSlide.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Card from "./Card";
+import { Autoplay, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import { ourPlans } from "@/data/dataPlans";
+
+export default function PlansSlide() {
+  return (
+    <div>
+      <Swiper
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 15,
+          },
+          600: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          1200: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+        }}
+        autoplay={{ delay: 5000 }}
+        pagination={{ clickable: true }}
+        modules={[Pagination, Autoplay]}
+      >
+        {ourPlans.map((plan) => (
+          <SwiperSlide key={plan.id}>
+            <Card title={plan.title} value={plan.value} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
---
+++ my-app/src/data/dataPlans.ts
@@ -0,0 +1,24 @@
+import { nanoid } from "nanoid";
+
+export const ourPlans = [
+  {
+    id: nanoid(5),
+    title: "50",
+    value: "99,99"
+  },
+  {
+    id: nanoid(5),
+    title: "100",
+    value: "149,99"
+  },
+  {
+    id: nanoid(5),
+    title: "200",
+    value: "199,99"
+  },
+  {
+    id: nanoid(5),
+    title: "300",
+    value: "249,99"
+  },
+]
