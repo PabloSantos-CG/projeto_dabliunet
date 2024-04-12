3608d42fa32a456ec9d5ca953b2ec04cf670a26f
PabloSantos-CG <comercial-pablo@outlook.com>
2024-04-12 15:32:03 -0300

    style: exchange fixed height for padding

--- my-app/src/components/OurPlans.tsx
+++ my-app/src/components/OurPlans.tsx
@@ -3,7 +3,7 @@ import PlansSlide from "./slide-components/PlansSlide";
 
 export default function OurPlans() {
   return(
-    <section id="OurPlans" className="h-[600px] flex justify-center items-center">
+    <section id="OurPlans" className="flex justify-center items-center py-32">
       <div className="container m-auto px-4">
         <h2 className="text-white text-3xl text-center font-semibold mb-12">Nossos Planos</h2>
         <PlansSlide />
