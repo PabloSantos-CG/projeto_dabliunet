ac176d374e14ececffd4746e89456a3242c75986
PabloSantos-CG <comercial-pablo@outlook.com>
2024-04-12 18:23:38 -0300

    style: refactor style

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -2,7 +2,7 @@ import Nav from "./header/Nav";
 
 export default function Header() {
   return (
-    <header className="bg-blue-800">
+    <header className="bg-blue-800 w-full fixed top-0 left-0 z-50">
       <div className="container flex justify-between m-auto px-4 py-5">
         <div>
           <a href="/" className="text-xl font-bold text-white">
