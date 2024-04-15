5f5bd93dfbb9ac1f6f3f2eb1fa8b515dde8b7224
PabloSantos-CG <comercial-pablo@outlook.com>
2024-04-15 16:18:17 -0300

    refactor: add effects

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,8 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Nav from "./header/Nav";
 
 export default function Header() {
+  const [header, setHeader] = useState(false);
+  
+  const handleHeader = () => {
+    if (window.scrollY >= 20) {
+      setHeader(true)
+    } else {
+      setHeader(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleHeader)
+    
+  },[]);
+
   return (
-    <header className="bg-blue-800 w-full fixed top-0 left-0 z-50">
+    <header
+      className={`w-full ${
+        header ? "fixed bg-[#082887]" : "bg-[#02091D]"
+      } top-0 z-50 ease-linear duration-500`}
+    >
       <div className="container flex justify-between m-auto px-4 py-5">
         <div>
           <a href="/" className="text-xl font-bold text-white">
