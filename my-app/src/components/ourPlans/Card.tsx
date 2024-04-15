d041ac958a3d94c20ad4545b241073cdd09f1aef
PabloSantos-CG <comercial-pablo@outlook.com>
2024-04-15 16:18:33 -0300

    feat: add card

---
+++ my-app/src/components/ourPlans/Card.tsx
@@ -0,0 +1,42 @@
+interface Props {
+  title: string;
+  value: string;
+}
+
+function Card({ title, value }: Props) {
+  return (
+    <div
+      className="
+        flex flex-col justify-between text-center
+        h-[340px] rounded-lg
+       bg-white
+       p-6
+      "
+    >
+      <strong className="text-4xl">
+        {title} <span className="text-2xl text-blue-800">MEGA</span>
+      </strong>
+
+      <ul className="flex flex-col gap-y-2 text-gray-500 text-sm">
+        <li>100% Fibra Óptica</li>
+        <li>Suporte 24h</li>
+        <li>Wifi-Ultra</li>
+      </ul>
+
+      <strong className="text-gray-500 text-lg">R$ {value} / Mês</strong>
+
+      <button
+        className="
+          w-full
+          bg-[#0CF25D] hover:bg-[#0ac64c] text-white 
+          py-1 rounded drop-shadow-md font-semibold
+          duration-500
+        "
+      >
+        EU QUERO
+      </button>
+    </div>
+  );
+}
+
+export default Card;
