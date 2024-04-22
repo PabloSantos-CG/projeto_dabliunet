bd6fd8a4e8ccd6f923179ad6a6e398223c9b812e
PabloSantos-CG <comercial-pablo@outlook.com>
2024-04-22 13:28:56 -0300

    feat: add menu hamburguer

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -2,22 +2,41 @@
 
 import { useEffect, useState } from "react";
 import Nav from "./header/Nav";
+import { IoClose, IoMenu } from "react-icons/io5";
 
 export default function Header() {
   const [header, setHeader] = useState(false);
-  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((value) => !value);
+    console.log("Valor trocado", isOpen);
+  };
+
+  const handleResize = () => {
+    if (window.innerWidth < 768 && isOpen) setIsOpen(false);
+    if (window.innerWidth > 768 && !isOpen) setIsOpen(true);
+  };
+
   const handleHeader = () => {
     if (window.scrollY >= 20) {
-      setHeader(true)
+      setHeader(true);
     } else {
       setHeader(false);
     }
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener("scroll", handleHeader)
-    
-  },[]);
+    window.addEventListener("scroll", handleHeader);
+    if (window.innerWidth > 768 && !isOpen) setIsOpen(true);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isOpen]);
 
   return (
     <header
@@ -32,7 +51,13 @@ export default function Header() {
           </a>
         </div>
 
-        <Nav />
+        <button
+          onClick={toggleMenu}
+          className="md:hidden absolute right-2 sm:top-5  max-sm:top-3 text-white"
+        >
+          <span>{isOpen ? <IoClose size={36} /> : <IoMenu size={36} />}</span>
+        </button>
+        <Nav toggleOpen={isOpen} />
       </div>
     </header>
   );
