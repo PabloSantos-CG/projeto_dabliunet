671d7e2b23d07d30da2927e7bfa193ac0de659e0
PabloSantos-CG <comercial-pablo@outlook.com>
2024-05-02 14:00:57 -0300

    feat: add last footer component

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -1,10 +1,14 @@
 import FirstFooter from "./footer/FirstFooter";
+import LastFoorter from "./footer/LastFoorter";
 
 export default function Footer() {
   return (
     <footer>
       <div className="bg-[#2D2D2D]">
         <FirstFooter />
+      </div>
+      <div className="bg-[#292828]">
+        <LastFoorter />
       </div>
     </footer>
   );
---
+++ my-app/src/components/footer/LastFoorter.tsx
@@ -0,0 +1,8 @@
+export default function LastFoorter() {
+  return (
+    <div className="container m-auto py-7 text-white text-center">
+      <a href="#">Política de privacidade</a>
+      <p className="pt-3">© Todos os direitos reservados - Desenvolvido por PabloSantos-CG</p>
+    </div>
+  );
+}
